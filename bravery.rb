606a76e9ccbde58653d5c8b568f356b3ca9c8f28
rick carlino <rick.carlino@gmail.com>
2013-12-21 23:33:34 -0700

    Works, need mongoid support

---
+++ bravery.rb
@@ -0,0 +1,53 @@
+module Bravery
+  module ClassMethods
+    @@states = []
+
+    def define_state(state_name, &callback)
+      @@states << state_name
+      define_method state_name, callback
+    end
+
+    def define_start_state(state_name, &callback)
+      state_name = state_name.to_sym
+      @@start_state = state_name
+      define_state(state_name, &callback)
+    end
+
+    def states
+      @@states
+    end
+
+    def start_state
+      @@start_state
+    end
+  end
+
+  module InstanceMethods
+
+    def states
+      self.class.states
+    end
+
+    def goto_state(state_name)
+      state_name = state_name.to_sym
+      set_state  state_name
+      self.send state_name
+    end
+
+    def state
+      if @state
+        @state
+      else
+        @state || self.class.start_state
+      end
+    end
+
+    def set_state(state_name)
+      @state = state_name.to_sym
+    end
+  end
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
---
+++ main.rb
@@ -0,0 +1,72 @@
+require 'pry'
+
+require_relative 'bravery'
+
+class Subject
+  include Bravery
+
+  attr_accessor :value
+
+  def initialize
+    @value = 'Not Yet Set.'
+  end
+
+  define_start_state :first do
+    @value= '1'
+  end
+
+  define_state :second do
+    @value = '2'
+    goto_state :third
+  end
+
+  define_state :third do
+    @value = '3'
+  end
+
+end
+
+require 'test/unit'
+
+class TestBravery < Test::Unit::TestCase
+
+  def setup
+    @test = Subject.new
+  end
+
+  def teardown
+    @test = nil
+  end
+
+  def test_value_attribute
+    assert_equal('Not Yet Set.', @test.value)
+  end
+
+  def test_default_state
+    assert_equal(:first, @test.state)
+  end
+
+  def test_states_list
+    assert_equal([:first, :second, :third], @test.states)
+  end
+
+  def test_goto_state
+    @test.goto_state :first
+    assert_equal('1', @test.value)
+    assert_equal(:first, @test.state)
+  end
+
+  def test_set_state
+    @test.set_state :third
+    assert_equal('Not Yet Set.', @test.value)
+    assert_equal(:third, @test.state)
+  end
+
+  def test_state_traversal
+    @test.goto_state :second
+    assert_equal('3', @test.value)
+    assert_equal(:third, @test.state)
+  end
+end
+
+TestBravery.test
